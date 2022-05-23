17187ad5670640cbf767429042232108502d59da
Katakung95 <tanwasripan@hotmail.co.uk>
2022-05-23 15:01:13 +0100

    part1 - anecdotes - step 2 to 3

--- part1/anecdotes/anecdotesapp/src/App.js
+++ part1/anecdotes/anecdotesapp/src/App.js
@@ -1,23 +1,43 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+	const anecdotes = [
+		'If it hurts, do it more often',
+		'Adding manpower to a late software project makes it later!',
+		'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+		'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+		'Premature optimization is the root of all evil.',
+		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+		'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
+	];
 
-  return (
-    <div>
-      {anecdotes[selected]}
-    </div>
-  )
-}
+	const [selected, setSelected] = useState(0);
+	const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
+  const [mostUpvotes, setMostUpvotes] = useState(0);
+	console.log(vote);
+	const randomise = () => {
+		const next = Math.floor(Math.random() * anecdotes.length);
+		setSelected(next);
+	};
+  const upvote = () => {
+    const newVotes = [...vote];
+    newVotes[selected]++;
+    const max = Math.max(...newVotes);
+    setMostUpvotes(newVotes.indexOf(max));
+    setVote(newVotes);
+  };
 
-export default App
+	return (
+		<>
+      <h1>Anecdote of the day</h1>
+			<div>{anecdotes[selected]}</div>
+      <p>has {vote[selected]} votes</p>
+      <button onClick={upvote}>vote</button>
+			<button onClick={randomise}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[mostUpvotes]}</div>
+		</>
+	);
+};
+
+export default App;
