02b38cd6a4553dc3b8094eef865776c1098f0107
Katakung95 <tanwasripan@hotmail.co.uk>
2022-05-24 19:52:33 +0100

    part2 - phonebook - removed key as prob to contact

--- part2/phonebook/phonebookapp/src/components/Contact.js
+++ part2/phonebook/phonebookapp/src/components/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function Contact({ name, tel, key }) {
+export default function Contact({ name, tel }) {
 	return (
-		<p key={key}>
+		<p>
 			{name} {tel}
 		</p>
 	);
