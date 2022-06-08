5825144ea2707e52cf7350b641b14709ba45eba3
Katakung95 <tanwasripan@hotmail.co.uk>
2022-06-08 22:12:09 +0100

    part4 - blog list expansion - step 6 to 8

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,19 +3,36 @@ const app = express();
 const cors = require('cors');
 const blogRouter = require('./controllers/blog');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.substring(7);
+    request.token = token;
+  }
+
+  next();
+};
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
+app.use(tokenExtractor);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 const errorHandler = (error, _request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   next(error);
--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 });
@@ -21,14 +22,20 @@ blogRouter.post('/', async (request, response, next) => {
     response.status(400).end();
   }
 
-  const user = await User.find({});
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  const blog = new Blog({ ...request.body, user: user[0].id });
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({ ...request.body, user: user._id });
 
   try {
     const result = await blog.save();
-    user[0].blogs = user[0].blogs.concat(result._id);
-    await user[0].save();
+    user.blogs = user.blogs.concat(result._id);
+    await user.save();
     response.status(201).json(result);
   } catch (exception) {
     next(exception);
